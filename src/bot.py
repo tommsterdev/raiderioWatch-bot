890d76562189b0cdbffcae738e6f31f0ce8e205f
tommstrdev <tomyuval8@gmail.com>
2024-02-13 21:21:01 -0500

    fixed request query params

--- src/bot.py
+++ src/bot.py
@@ -12,8 +12,8 @@ import logging
 intents = discord.Intents.default()
 intents.message_content = True
 load_dotenv()
-TOKEN = os.getenv('BOT_TOKEN')
-URL_ENDPOINT = os.getenv('API_URL')
+TOKEN = os.getenv("BOT_TOKEN")
+URL_ENDPOINT = os.getenv("API_URL")
 
 
 client = discord.Client(intents=intents)
@@ -21,30 +21,31 @@ client = discord.Client(intents=intents)
 http = urllib3.PoolManager(num_pools=10)
 
 
-
 @client.event
 async def on_ready() -> None:
-    print(f'We have logged in as {client.user}')
+    print(f"We have logged in as {client.user}")
+
 
 @client.event
 async def on_message(message: discord.Message) -> None:
     if message.author == client.user:
         return
-    
-    if message.content.startswith('!rat'):
-        percent_rat = random.randint(1,101)
-        result = f"{message.author.nick} is {percent_rat}% rat" 
+
+    if message.content.startswith("!rat"):
+        percent_rat = random.randint(1, 101)
+        result = f"{message.author.nick} is {percent_rat}% rat"
         await message.channel.send(result)
-    
-    if message.content.startswith('!'):
+
+    if message.content.startswith("!"):
         result = await parse_command(message.content)
-        if not result: return
+        if not result:
+            return
         await message.channel.send(result)
 
 
 async def parse_command(message: str) -> str:
     if len(message) == 1:
-        return ''
+        return ""
     # remove '!' and parse into words
     parsed_message = message[1:].split()
     command: str = parsed_message[0]
@@ -52,91 +53,98 @@ async def parse_command(message: str) -> str:
     result = f"received command {command} with arguments {args}"
     match command:
 
-        case 'score':
+        case "score":
             result = await get_score(args)
 
-        case 'leaderboard':
+        case "leaderboard":
             result = await get_leaderboard()
 
-        case 'ilvl':
+        case "ilvl":
             result = await get_ilvl(args)
 
-        case 'best':
+        case "best":
             result = await get_best_run(args)
 
-        case 'twr':
+        case "twr":
             result = await get_best_weekly_run(args)
 
         case _:
-            result = ''    
+            result = ""
 
     return result
+
 
 async def get_score(args: List[str]) -> str:
     res = await request_score(args)
-    return f"{res['name'].capitalize()}, {res['realm'].capitalize()} m+ score {res['score']}"
+    return f"{res['character'].capitalize()}, {res['realm'].capitalize()} m+ score {res['score']}"
+
 
 async def get_leaderboard() -> str:
-    return f'not implemented'
+    return f"not implemented"
+
 
 async def get_ilvl(args: List[str]) -> str:
-    return f'not implemented for args={args}'
+    return f"not implemented for args={args}"
+
 
 async def get_best_run(args: List[str]) -> str:
-    return f'not implemented for args={args}'
+    return f"not implemented for args={args}"
+
 
 async def get_best_weekly_run(args: List[str]) -> str:
-    return f'not implemented for args={args}'
+    return f"not implemented for args={args}"
 
 
 async def request_score(query: List[str]) -> Dict[str, Any]:
-    if len(query) < 2:
+    if len(query) < 1:
         return {
-                'statusCode': 401,
-                'error': 'bad request',
-            }
-    name = query[0]
-    realm = query[1]
-    body = {'name' : name, 'realm': realm}
-    params = json.dumps(body)
+            "statusCode": 401,
+            "error": "bad request",
+        }
+    elif len(query) < 2:
+        realm = "Tichondrius"
+    else:
+        realm = query[1].capitalize()
+    character = query[0].capitalize()
     # TODO: set API for query parameters
-    #endpoint = f'{URL_ENDPOINT}?name={name}&realm={realm}'
-    endpoint = URL_ENDPOINT
+    # endpoint = f'{URL_ENDPOINT}?name={name}&realm={realm}'
+    endpoint = f"{URL_ENDPOINT}?character={character}&realm={realm}"
     print(endpoint)
 
     try:
-        response = http.request(method='GET', url=endpoint, body=params ,headers={'Content-Type': 'application/json'})
+        response = http.request(
+            method="GET", url=endpoint, headers={"Content-Type": "application/json"}
+        )
+
         if response.status != 200:
-            data = json.loads(response.data.decode('utf-8'))
+            data = json.loads(response.data.decode("utf-8"))
             print(data)
-            logging.info(f'response status code={response.status}, {data}')
+            logging.info(f"response status code={response.status}, {data}")
             return {
-                'statusCode': 401,
-                'error': 'bad request to api',
+                "statusCode": 401,
+                "error": "bad request to api",
             }
         # decode
 
-        data = json.loads(response.data.decode('utf-8'))
+        data = json.loads(response.data.decode("utf-8"))
 
-        score = data.get('score')
+        score = data.get("score")
 
     except exceptions.HTTPError as e:
         return {
-            'statusCode' : 400,
-            'Error' : str(e),
+            "statusCode": 400,
+            "Error": str(e),
         }
     else:
-        region = data.get('region', 'us')
+        region = data.get("region", "us")
         # construct response object
         result = {
-            'name' : name,
-            'realm' : realm,
-            'region' : region,
-            'score' : score,
+            "character": character,
+            "realm": realm,
+            "region": region,
+            "score": score,
         }
     return result
-    
-
 
 
 client.run(TOKEN)
